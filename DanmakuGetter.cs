d70aa1d2d3cc43054a7e6a7ab78b170ccd262fc8
Baka632 <48171809+Baka632@users.noreply.github.com>
2020-12-06 17:07:19 +0800

    添加项目文件。

---
+++ DanmakuGetter.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+using Windows.Networking.BackgroundTransfer;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.UI.Xaml.Controls;
+
+namespace BilibiliDanmakuDownloader
+{
+    class DanmakuGetter : DependencyObject
+    {
+        static AppInfomation appInfomation = App.appInfomation;
+        public async void GetDanmaku(long aid, StorageFile saveFile)
+        {
+            try
+            {
+                if (aid < 0)
+                {
+                    throw new ArgumentException();
+                }
+                BackgroundDownloader downloader = new BackgroundDownloader();
+                DownloadOperation downloadRawCidData = downloader.CreateDownload(new Uri($"https://www.bilibili.com/widget/getPageList?aid={aid}"), await ApplicationData.Current.TemporaryFolder.CreateFileAsync(aid.ToString()));
+                await downloadRawCidData.StartAsync();
+                StorageFile jsonFile = await ApplicationData.Current.TemporaryFolder.GetFileAsync(aid.ToString());
+                string rawCidData = File.ReadAllText(jsonFile.Path).Replace("[", "").Replace("]", "").Trim();
+                JObject jObject = JObject.Parse(rawCidData);
+                JProperty jProperty = jObject.Property("cid");
+                int cid = int.Parse(jProperty.Value.ToString());
+                await jsonFile.DeleteAsync();
+
+
+                string site = $"http://comment.bilibili.com/{cid}.xml";
+                DownloadOperation download = downloader.CreateDownload(new Uri(site), saveFile);
+                await download.StartAsync();
+                ShowInfoBar(saveFile.Path);
+            }
+            catch (Exception ex)
+            {
+                ShowInfoBar(ex);
+            }
+        }
+
+        private static void ShowInfoBar(Exception exception)
+        {
+            if (exception.HResult == -2145844844)
+            {
+                appInfomation.InfoBarSeverity = InfoBarSeverity.Error;
+                appInfomation.InfoBarTitle = "无效的ID";
+                appInfomation.InfoBarMessage = "请重试。";
+                appInfomation.IsInfoBarOpen = true;
+            }
+            else
+            {
+                appInfomation.InfoBarSeverity = InfoBarSeverity.Error;
+                appInfomation.InfoBarTitle = "出现问题";
+                appInfomation.InfoBarMessage = exception.Message;
+                appInfomation.IsInfoBarOpen = true;
+            }
+        }
+
+        private static void ShowInfoBar(string path)
+        {
+            appInfomation.InfoBarSeverity = InfoBarSeverity.Success;
+            appInfomation.InfoBarTitle = "保存成功";
+            appInfomation.InfoBarMessage = $"文件已保存为{path}";
+            appInfomation.IsInfoBarOpen = true;
+        }
+
+        public async void GetDanmaku(long cid,bool IsCid,StorageFile saveFile)
+        {
+            if (IsCid)
+            {
+                if (cid < 0)
+                {
+                    throw new ArgumentException();
+                }
+                try
+                {
+                    BackgroundDownloader downloader = new BackgroundDownloader();
+                    string site = $"http://comment.bilibili.com/{cid}.xml";
+                    DownloadOperation download = downloader.CreateDownload(new Uri(site), saveFile);
+                    await download.StartAsync();
+                    ShowInfoBar(saveFile.Path);
+                }
+                catch (Exception ex)
+                {
+                    ShowInfoBar(ex);
+                }
+            }
+            else
+            {
+                GetDanmaku(cid, saveFile);
+            }
+        }
+
+        public async void GetDanmaku(string bvid, StorageFile saveFile)
+        {
+            try
+            {
+                BackgroundDownloader downloader = new BackgroundDownloader();
+                DownloadOperation downloadRawCidData = downloader.CreateDownload(new Uri($"https://api.bilibili.com/x/player/pagelist?bvid={bvid}"), await ApplicationData.Current.TemporaryFolder.CreateFileAsync(bvid));
+                await downloadRawCidData.StartAsync();
+                StorageFile jsonFile = await ApplicationData.Current.TemporaryFolder.GetFileAsync(bvid);
+                string rawCidData = File.ReadAllText(jsonFile.Path).Replace("[", "").Replace("]", "").Trim();
+                JObject jObject = JObject.Parse(rawCidData);
+                JProperty jProperty = jObject.Property("cid");
+                int cid = int.Parse(jProperty.Value.ToString());
+                await jsonFile.DeleteAsync();
+
+                string site = $"http://comment.bilibili.com/{cid}.xml";
+                string fileName = $"{cid}.xml";
+                DownloadOperation download = downloader.CreateDownload(new Uri(site), saveFile);
+                await download.StartAsync();
+                ShowInfoBar(saveFile.Path);
+            }
+            catch (Exception ex)
+            {
+                ShowInfoBar(ex);
+            }
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace BilibiliDanmakuDownloader
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        DanmakuGetter danmakuGetter = new DanmakuGetter();
+        bool IsStarting = true;
+        AppInfomation appInfomation = App.appInfomation;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            IdProperty = idComboBox.SelectedItem as string;
+            IsStarting = false;
+        }
+
+        public string IdProperty { get; set; }
+
+        private void GetDanmaku(object sender, RoutedEventArgs e)
+        {
+            GetSaveFile();
+        }
+
+        private async void GetSaveFile()
+        {
+            FileSavePicker fileSavePicker = new FileSavePicker();
+            fileSavePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            fileSavePicker.FileTypeChoices.Add("XML文件", new List<string>() { ".xml" });
+            switch (IdProperty)
+            {
+                case "AV号":
+                    long aid;
+                    if (long.TryParse(idTextBox.Text.ToLower().Replace("av", "").Trim(), out aid))
+                    {
+                        fileSavePicker.SuggestedFileName = $"{aid}";
+                        StorageFile file = await fileSavePicker.PickSaveFileAsync();
+                        if (file != null)
+                        {
+                            danmakuGetter.GetDanmaku(aid, file);
+                        }
+                        else
+                        {
+                            appInfomation.InfoBarSeverity = Microsoft.UI.Xaml.Controls.InfoBarSeverity.Warning;
+                            appInfomation.InfoBarTitle = "已取消操作";
+                            appInfomation.InfoBarMessage = "";
+                            appInfomation.IsInfoBarOpen = true;
+                        }
+                    }
+                    break;
+                case "CID":
+                    long cid;
+                    if (long.TryParse(idTextBox.Text, out cid))
+                    {
+                        fileSavePicker.SuggestedFileName = $"{cid}";
+                        await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
+                        {
+                            StorageFile storageFile = await fileSavePicker.PickSaveFileAsync();
+                            if (storageFile != null)
+                            {
+                                danmakuGetter.GetDanmaku(cid, storageFile);
+                            }
+                            else
+                            {
+                                appInfomation.InfoBarSeverity = Microsoft.UI.Xaml.Controls.InfoBarSeverity.Warning;
+                                appInfomation.InfoBarTitle = "已取消操作";
+                                appInfomation.InfoBarMessage = "已取消操作";
+                                appInfomation.IsInfoBarOpen = true;
+                            }
+                        });
+                    }
+                    else
+                    {
+                        getDanmakuButton.IsEnabled = false;
+                    }
+                    break;
+                case "BV号":
+                    string bvid = idTextBox.Text;
+                    fileSavePicker.SuggestedFileName = $"{bvid}";
+                    StorageFile storageFile1 = await fileSavePicker.PickSaveFileAsync();
+                    if (storageFile1 != null)
+                    {
+                        danmakuGetter.GetDanmaku(bvid, storageFile1);
+                    }
+                    else
+                    {
+                        appInfomation.InfoBarSeverity = Microsoft.UI.Xaml.Controls.InfoBarSeverity.Warning;
+                        appInfomation.InfoBarTitle = "已取消操作";
+                        appInfomation.InfoBarMessage = "已取消操作";
+                        appInfomation.IsInfoBarOpen = true;
+                    }
+                    break;
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selection = e.AddedItems[0].ToString();
+            IdProperty = selection;
+            if (!IsStarting)
+            {
+                CheckidTextBoxValue();
+            }
+        }
+
+        private void idTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckidTextBoxValue();
+        }
+
+        private void CheckidTextBoxValue()
+        {
+            if (string.IsNullOrWhiteSpace(idTextBox.Text))
+            {
+                getDanmakuButton.IsEnabled = false;
+            }
+            else if (long.TryParse(idTextBox.Text.ToLower().Replace("av", "").Trim(), out _) == false && IdProperty != "BV号")
+            {
+                getDanmakuButton.IsEnabled = false;
+            }
+            else
+            {
+                getDanmakuButton.IsEnabled = true;
+            }
+        }
+    }
+}
