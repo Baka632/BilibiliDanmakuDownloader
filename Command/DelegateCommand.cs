37b2fd925f832b1e0551d7318e26540cae45d738
Baka632 <48171809+Baka632@users.noreply.github.com>
2021-07-09 22:49:38 +0800

    更新WinUI库,添加DelegateCommand类

---
+++ Command/DelegateCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BilibiliDanmakuDownloader.Command
+{
+    public class DelegateCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        public Action<object> ExecuteAction { get; set; }
+        public Func<object, bool> CanExecuteFunc { get; set; }
+
+        public DelegateCommand()
+        {
+
+        }
+
+        public DelegateCommand(Action<object> action)
+        {
+            ExecuteAction = action;
+        }
+
+        public DelegateCommand(Action<object> action, Func<object, bool> func)
+        {
+            ExecuteAction = action;
+            CanExecuteFunc = func;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (CanExecuteFunc == null)
+            {
+                return true;
+            }
+            return CanExecuteFunc(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            if (ExecuteAction == null)
+            {
+                return;
+            }
+            ExecuteAction(parameter);
+        }
+    }
+}
